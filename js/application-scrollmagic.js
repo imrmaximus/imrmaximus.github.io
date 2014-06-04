baa905830fe4e80760fa80eb4b00cc47e235580d
imrmaximus <maxime_avon@icloud.com>
2014-06-04 17:52:32 +0200

    MAJ pour le CV dynamique : Suppression des données de débugage. MAJ des heures de démarrage des événements et de leur durée pour améliorer l'expérience.

--- js/application-scrollmagic.js
+++ js/application-scrollmagic.js
@@ -8,34 +8,32 @@ $(document).ready(function() {
     var controller = new ScrollMagic();
 
 	// build scene
-    scene = new ScrollScene({triggerElement: "#marker-debut", duration: windowHeight*3})
+    scene = new ScrollScene({triggerElement: "#marker-debut", duration: windowHeight*6})
         .setPin("#main")
-        .addTo(controller)
-        .addIndicators();
+        .addTo(controller);
 
     var tween = new TimelineMax()
-        .add(TweenMax.fromTo("#pres-contact", 5, {top: -300}, {top: '30vh', ease: Back.easeInOut}))
+        .add(TweenMax.fromTo("#pres-contact", 20, {top: -300}, {top: '30vh', ease: Back.easeInOut}))
         .add(TweenMax.to("#photo-profil", 5, {left: 100}))
         .add(TweenMax.to("#contacts", 5, {left: -150, opacity: 1}))
         .add(TweenMax.to("#title-CV", 5, {opacity: 1}))
         .add(TweenMax.to("#its-me", 5, {opacity: 0}))
         .add(TweenMax.to("#its-me", 5, {left: 400, top: 5}));
 
-    scene = new ScrollScene({triggerElement: "#marker-debut", duration: 300})
+    scene = new ScrollScene({triggerElement: "#marker-debut", duration: 500})
         .setTween(tween)
         .addTo(controller);
 
     tween = new TimelineMax()
         .add(TweenMax.to("#contacts", 5, {left: -500, opacity: 0}));
 
-    scene = new ScrollScene({triggerElement: "#marker-contact-away", duration: windowHeight*1/10})
+    scene = new ScrollScene({triggerElement: "#marker-contact-away", duration: 300})
         .setTween(tween)
-        .addTo(controller)
-        .addIndicators();
+        .addTo(controller);
 
     tween = TweenMax.to("#photo-arbre-ciel", 5, {top: -1000});
 
-    scene = new ScrollScene({triggerElement: "#marker-technicals", duration: 300})
+    scene = new ScrollScene({triggerElement: "#marker-technicals", duration: 500})
         .setTween(tween)
         .addTo(controller);
 
@@ -44,20 +42,18 @@ $(document).ready(function() {
         .add(TweenMax.to("#photo-profil", 5, {left: '150%'}))
         .add(TweenMax.fromTo("#technicals", 5, {left: -500, opacity: 0}, {left: -60, opacity: 1}));
 
-    scene = new ScrollScene({triggerElement: "#marker-technicals", duration: 300})
+    scene = new ScrollScene({triggerElement: "#marker-technicals", duration: 500})
         .setTween(tween)
-        .addTo(controller)
-        .addIndicators();
+        .addTo(controller);
 
     tween = new TimelineMax()
         .add(TweenMax.to("#technicals", 5, {opacity: 0}))
         .add(TweenMax.to("#photo-haunted-mansion", 5, {left: -1100}))
         .add(TweenMax.to("#professional-history", 5, {css:{zIndex: 53}}));
 
-    scene = new ScrollScene({triggerElement: "#marker-history", duration: 300})
+    scene = new ScrollScene({triggerElement: "#marker-history", duration: 500})
         .setTween(tween)
-        .addTo(controller)
-        .addIndicators();
+        .addTo(controller);
 
     tween = new TimelineMax()
         .add(TweenMax.to("#photo-profil", 5, {top: 0}))
@@ -65,8 +61,7 @@ $(document).ready(function() {
 
     scene = new ScrollScene({triggerElement: "#marker-history", duration: 300})
         .setTween(tween)
-        .addTo(controller)
-        .addIndicators();
+        .addTo(controller);
 
     tween = new TimelineMax()
         .add(TweenMax.to("#photo-dune", 1, {css:{zIndex: 54}}))
@@ -77,8 +72,7 @@ $(document).ready(function() {
 
     scene = new ScrollScene({triggerElement: "#marker-training", duration: 300})
         .setTween(tween)
-        .addTo(controller)
-        .addIndicators();
+        .addTo(controller);
 
     tween = new TimelineMax()
         .add(TweenMax.to("#training", 5, {opacity: 0}))
@@ -90,8 +84,7 @@ $(document).ready(function() {
 
     scene = new ScrollScene({triggerElement: "#marker-show-various", duration: 300})
         .setTween(tween)
-        .addTo(controller)
-        .addIndicators();
+        .addTo(controller);
 
     tween = new TimelineMax()
         .add(TweenMax.to("#photo-moi", 5, {opacity: 1}))
@@ -115,18 +108,7 @@ $(document).ready(function() {
 
     scene = new ScrollScene({triggerElement: "#marker-et-voila", duration: 300})
         .setTween(tween)
-        .addTo(controller)
-        .addIndicators();
-
-    /*tween = new TimelineMax()
-        .add(TweenMax.to("#training", 5, {opacity: 1}));
-
-    scene = new ScrollScene({triggerElement: "#marker-show-training", duration: 300})
-        .setTween(tween)
-        .addTo(controller)
-        .addIndicators();
-
-    marker-show-various*/
+        .addTo(controller);
 
 });
 
